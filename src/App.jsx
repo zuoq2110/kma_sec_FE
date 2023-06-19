01b1225c0b695dd93fb6b81fd33b1402e01d79f7
hieubm <hieubui00.kma@gmail.com>
2023-06-19 20:43:50 +0700

    feat: import primereact into project

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
-import "./styles/assets/css/atlantis.css";
-import "./styles/assets/css/fonts.css";
-import "./styles/assets/css/atlantis.min.css";
-import "./styles/assets/css/bootstrap.min.css";
-import "./styles/assets/css/fonts.min.css";
-
-//theme
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-
-//core
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
 import { privateRoutes } from "./routes/privateRoutes";
-
 import DataProvider from "./context/dataContext";
 
-function App() {
+import "primereact/resources/themes/mdc-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import "./index.css";
+import "./flags.min.css";
+
+export default function App() {
   return (
     <Router>
       <DataProvider>
@@ -30,20 +19,6 @@ function App() {
           ))}
         </Routes>
       </DataProvider>
-      <ToastContainer
-        position="bottom-right"
-        autoClose={1000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
     </Router>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+
 root.render(
   <Provider store={store}>
     <App />
