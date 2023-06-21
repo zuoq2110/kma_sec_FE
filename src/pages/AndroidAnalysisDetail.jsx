a8502349fb3bb7b28649e62ee707a1a0e5d9648a
hieubm <hieubui00.kma@gmail.com>
2023-06-21 11:41:31 +0700

    feat(android-analysis-details): update ui components

--- src/pages/AndroidAnalysisDetail.jsx
+++ src/pages/AndroidAnalysisDetail.jsx
@@ -1,69 +1,77 @@
-import React, { useEffect, useState } from "react";
-
+import React, { useState, useRef, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import ToastNotification from "../components/ToastMessage";
+import { Toast } from "primereact/toast";
 import { Divider } from "primereact/divider";
 import { BreadCrumb } from "primereact/breadcrumb";
 import { Button } from "primereact/button";
+import { Accordion, AccordionTab } from "primereact/accordion";
 
-const BASE_URL = process.env.REACT_APP_KSECURITY_SERVICE_URL;
+const KSECURITY_URL = "http://14.225.205.142:8000";
 
 const AndroidAnalysisDetail = () => {
-  const [dataAnalysis, setDataAnalysis] = useState();
+  const [analysisDetails, setAnalysisDetails] = useState();
   const path = useLocation();
-  const pathNames = path.pathname.replace("/analysis/android/", "").trim();
-  const analysisId = pathNames.replace("/", "").trim();
+  const toast = useRef(null);
 
-  const home = { icon: "pi pi-home", url: "/" };
-  const items = [
-    { label: "Analysis" },
-    { label: "Android" },
-    { label: `${analysisId}` },
-  ];
+  function getAnalysisDetails(id) {
+    const url = `${KSECURITY_URL}/api/v1/android/applications/${id}`;
 
-  function notify(message, type) {
-    const content = {
-      title: "K-Security",
-      message: message,
-      // icon: "flaticon-alarm-1",
-    };
-
-    ToastNotification(content, type);
+    return fetch(url, { method: "GET" }).then((response) => response.json());
   }
 
   function save(analysisId) {
-    fetch(`${BASE_URL}/api/v1/android/applications/${analysisId}`, {
-      method: "GET",
-    })
-      .then((response) => response.json())
+    getAnalysisDetails(analysisId)
       .then((response) => {
-        const json = JSON.stringify(response);
+        const json = JSON.stringify(response.data);
         const blob = new Blob([json], { type: "application/json" });
         const link = document.createElement("a");
 
         link.href = window.URL.createObjectURL(blob);
         link.download = "analysis";
         link.click();
-        notify("Export application's analysis successfully!", "success");
+        toast.current.show({
+          severity: "success",
+          summary: "Success",
+          detail: "Export successfully!",
+        });
       })
-      .catch((error) => notify(error, "error"));
+      .catch((error) => {
+        toast.current.show({
+          severity: "error",
+          summary: "Failure",
+          detail: error,
+        });
+      });
   }
 
+  const pathNames = path.pathname.split("/").slice(1);
+  const home = { icon: "pi pi-home", url: "/" };
+  const items = pathNames.map((name) => {
+    return { label: name.charAt(0).toUpperCase() + name.slice(1) };
+  });
+
+  const summaryFeatures = [
+    "user_features",
+    "permissions",
+    "activities",
+    "services",
+    "receivers",
+  ];
+
   useEffect(() => {
-    fetch(`${BASE_URL}/api/v1/android/applications/${analysisId}`, {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        setDataAnalysis(response);
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const analysisId = pathNames.at(-1).trim();
+
+    getAnalysisDetails(analysisId).then((response) =>
+      setAnalysisDetails(response.data)
+    );
   }, []);
 
   return (
     <>
-      <div className="flex flex-wrap gap-2 align-items-center mb-4">
-        <h2 className="mr-3">Analysis</h2>
+      <Toast ref={toast}></Toast>
+
+      <div className="flex flex-wrap gap-2 align-items-center mb-5">
+        <h3 className="mr-3 mb-0">Analysis</h3>
         <Divider layout="vertical" />
         <BreadCrumb
           model={items}
@@ -71,160 +79,66 @@ const AndroidAnalysisDetail = () => {
           style={{ background: "transparent", border: 0 }}
         />
       </div>
-      <div className="main-panel">
-        {dataAnalysis && (
-          <div className="container-analysis-detail">
-            <div className="col-12 col-lg-10 col-xl-9">
-              <div className="row align-items-center title-analysis-detail">
-                <div className="col">
-                  <h6 className="page-title">{""}</h6>
-                  <h4 className="page-title">ID: #{dataAnalysis.data.id}</h4>
-                </div>
-                <div className="col-auto">
-                  <Button label="Save" onClick={() => save(analysisId)} />
-                </div>
-              </div>
-              <Divider layout="horizontal" />
-              <div className="row">
-                <div className="col-md-12">
-                  <div className="card card-invoice" style={{ width: "900px" }}>
-                    <div className="card-header">
-                      <div className="invoice-header">
-                        <h3 className="invoice-title">
-                          Name: {dataAnalysis.data.name}
-                        </h3>
-                        <h3 className="invoice-title">
-                          <strong>{dataAnalysis.data.malware_type}</strong>
-                        </h3>
-                      </div>
-                      <div className="invoice-desc"></div>
-                    </div>
-                    <div className="card-body">
-                      <Divider layout="horizontal" />
-                      <div className="row flex">
-                        <div className="col-md-4 info-invoice">
-                          <h5 className="sub">Version</h5>
-                          <h5>{dataAnalysis.data.version_name}</h5>
-                        </div>
-                        <div className="col-md-4 info-invoice">
-                          <h5 className="sub">Package ID</h5>
-                          <h5>{dataAnalysis.data.package}</h5>
-                        </div>
-                        <div className="col-md-4 info-invoice">
-                          <h5 className="sub">Version Code</h5>
-                          <h5>{dataAnalysis.data.version_code}</h5>
-                        </div>
-                      </div>
 
-                      <div className="row pt-3">
-                        <div className="col-md-12">
-                          <div className="invoice-detail">
-                            <div className="invoice-top">
-                              <h3 className="title">
-                                <strong>Summary</strong>
-                              </h3>
-                            </div>
-                            <div className="invoice-item">
-                              <div className="table-responsive">
-                                <table className="table table-striped">
-                                  <thead>
-                                    <tr>
-                                      <td className="text-center">
-                                        <strong>Fields</strong>
-                                      </td>
-                                      <td className="text-center">
-                                        <strong>Values</strong>
-                                      </td>
-                                    </tr>
-                                  </thead>
-                                  <tbody>
-                                    <tr>
-                                      <td className="text-center">
-                                        <strong>User Features</strong>
-                                      </td>
-                                      <td>
-                                        <br />
-                                        {dataAnalysis.data.user_features.map(
-                                          (user_feature, index) => (
-                                            <div key={index}>
-                                              {user_feature}
-                                            </div>
-                                          )
-                                        )}
-                                        <br />
-                                      </td>
-                                    </tr>
-                                    <tr>
-                                      <td className="text-center">
-                                        <strong>Permissions</strong>
-                                      </td>
-                                      <td>
-                                        <br />
-                                        {dataAnalysis.data.permissions.map(
-                                          (permission, index) => (
-                                            <div key={index}>{permission}</div>
-                                          )
-                                        )}
-                                        <br />
-                                      </td>
-                                    </tr>
-                                    <tr>
-                                      <td className="text-center">
-                                        <strong>Activities</strong>
-                                      </td>
-                                      <td>
-                                        <br />
-                                        {dataAnalysis.data.activities.map(
-                                          (activitie, index) => (
-                                            <div key={index}>{activitie}</div>
-                                          )
-                                        )}
-                                        <br />
-                                      </td>
-                                    </tr>
-                                    <tr>
-                                      <td className="text-center">
-                                        <strong>Services</strong>
-                                      </td>
-                                      <td>
-                                        <br />
-                                        {dataAnalysis.data.services.map(
-                                          (service, index) => (
-                                            <div key={index}>{service}</div>
-                                          )
-                                        )}
-                                        <br />
-                                      </td>
-                                    </tr>
-                                    <tr>
-                                      <td className="text-center">
-                                        <strong>Receivers</strong>
-                                      </td>
-                                      <td>
-                                        <br />
-                                        {dataAnalysis.data.receivers.map(
-                                          (receiver, index) => (
-                                            <div key={index}>{receiver}</div>
-                                          )
-                                        )}
-                                        <br />
-                                      </td>
-                                    </tr>
-                                  </tbody>
-                                </table>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+      {analysisDetails && (
+        <>
+          <div className="flex flex-wrap justify-content-between align-items-center mb-4">
+            <h5 className="mb-0">ID: #{analysisDetails.id}</h5>
+            <Button label="Save" onClick={() => save(analysisDetails.id)} />
+          </div>
+
+          <div className="card px-6 mb-5">
+            <div className="flex flex-wrap justify-content-between align-items-center mx-3 mt-2 mb-5">
+              <p className="my-0" style={{ fontSize: "1.5rem" }}>
+                Name: {analysisDetails.name}
+              </p>
+              <h4 className="my-0">{analysisDetails.malware_type}</h4>
+            </div>
+
+            <Divider />
+
+            <div className="flex flex-wrap justify-content-around align-items-center my-5">
+              <div>
+                <p>
+                  <strong>Version</strong>
+                </p>
+                <p>{analysisDetails.version_name}</p>
+              </div>
+              <div>
+                <p>
+                  <strong>Package ID</strong>
+                </p>
+                <p>{analysisDetails.package}</p>
+              </div>
+              <div>
+                <p>
+                  <strong>Version Code</strong>
+                </p>
+                <p>{analysisDetails.version_code}</p>
               </div>
             </div>
+
+            <h5 className="mx-3">Summary</h5>
+
+            <Accordion multiple activeIndex={[1]}>
+              {summaryFeatures.map((field) => {
+                return (
+                  <AccordionTab
+                    header={field
+                      .split("_")
+                      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                      .join(" ")}
+                  >
+                    {analysisDetails[field].map((value) => (
+                      <p className="m-0">- {value}</p>
+                    ))}
+                  </AccordionTab>
+                );
+              })}
+            </Accordion>
           </div>
-        )}
-      </div>
+        </>
+      )}
     </>
   );
 };
